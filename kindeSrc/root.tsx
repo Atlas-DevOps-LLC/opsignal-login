2d24e38e63d106b2014f17871f268be57f3c0727
mattjstrauss <mattjstrauss@gmail.com>
2026-02-24 15:06:25 -0700

    Attempt number 1000000

--- kindeSrc/root.tsx
+++ kindeSrc/root.tsx
@@ -29,10 +29,13 @@ export const Root = ({
         <meta content="nopagereadaloud" name="google" />
         
         <title>{context.widget.content.page_title}</title>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet" /> */}
         <link href={getSVGFaviconUrl()} rel="icon" type="image/svg+xml" />
+
+        {/* Import Google Fonts directly */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
+        
         {getKindeRequiredCSS()}
         {getKindeRequiredJS()}
         <style>{getStyles()}</style>
