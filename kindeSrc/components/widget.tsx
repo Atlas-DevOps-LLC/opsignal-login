793395a82da2a42e3124c2d9ff259523bbc389d3
mattjstrauss <mattjstrauss@gmail.com>
2026-02-24 14:29:37 -0700

    Trying to fix the widget styles

--- kindeSrc/components/widget.tsx
+++ kindeSrc/components/widget.tsx
@@ -7,13 +7,15 @@ const styles: {
   loginForm: React.CSSProperties;
   heading: React.CSSProperties;
   description: React.CSSProperties;
+  widget: React.CSSProperties;
 } = {
   loginForm: {
-    minWidth: "400px",
+    minWidth: "50%",
     margin: "0 auto",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
+    color: "white",
   },
   heading: {
     fontWeight: "600",
@@ -22,12 +24,16 @@ const styles: {
   description: {
     marginBottom: "1.5rem",
   },
+  widget: {
+    maxWidth: "448px",
+    margin: "0 auto",
+  },
 };
 
 export const Widget = (props: { heading: string; description: string }) => {
   return (
     <main style={styles.loginForm}>
-      <div style={{ padding: "2rem" }}>
+      <div style={styles.widget}>
         <h2 style={styles.heading}>{props.heading}</h2>
         <p style={styles.description}>{props.description}</p>
         {getKindeWidget()}
