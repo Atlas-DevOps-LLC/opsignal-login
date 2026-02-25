24668aee21addb8b693cd0dd5ce7751f0fe269fe
mattjstrauss <mattjstrauss@gmail.com>
2026-02-25 13:50:27 -0700

    Adjusting urls

--- kindeSrc/root.tsx
+++ kindeSrc/root.tsx
@@ -32,7 +32,7 @@ export const Root = ({
         <link href={getSVGFaviconUrl()} rel="icon" type="image/svg+xml" />
         {getKindeRequiredCSS()}
         {getKindeRequiredJS()}
-        <link href="https://dashboard.opsignal.ai/styles/fonts.css" rel="stylesheet" />
+        <link href="https://dashboard.sitescene.dev/styles/fonts.css" rel="stylesheet" />
         <style>{getStyles()}</style>
       </head>
 
