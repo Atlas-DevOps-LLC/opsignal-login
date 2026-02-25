e5a913934bfb0e2fed2828cca8278ed3f2859314
mattjstrauss <mattjstrauss@gmail.com>
2026-02-25 11:14:55 -0700

    Testing image

--- kindeSrc/layouts/default.tsx
+++ kindeSrc/layouts/default.tsx
@@ -68,8 +68,7 @@ const styles: {
   sidePanel: {
     backgroundColor: "rgba(0, 167, 227, 1)",
     backgroundImage: "linear-gradient(90deg in oklab, rgb(6, 89, 134) 0%, rgb(0, 167, 227) 100%)",
-    flex: 1,
-    maxWidth: "1024px",
+    width: "50%"
   },
   backgroundImageBlock: {
     display: "flex",
