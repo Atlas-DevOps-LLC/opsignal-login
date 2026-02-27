2ee186f2d7c75d9f73b0c40ab8b657dfa70b4a55
mattjstrauss <mattjstrauss@gmail.com>
2026-02-27 10:20:05 -0700

    Testing font styles

--- kindeSrc/styles/styles.ts
+++ kindeSrc/styles/styles.ts
@@ -1,7 +1,7 @@
 // CSS Variables configuration
 const kindeVariables = {
   baseFontFamily:
-    "-apple-system, system-ui, BlinkMacSystemFont, Helvetica, Arial, Segoe UI, Roboto, sans-serif",
+    "Figtree, -apple-system, system-ui, BlinkMacSystemFont, Helvetica, Arial, Segoe UI, Roboto, sans-serif",
   controlSelectTextBorderRadius: "8px",
   buttonPrimaryBackgroundColor: "rgba(0, 167, 227, 1)",
   buttonPrimaryBackgroundColorHover: "#026aa2",
@@ -25,7 +25,7 @@ export const getStyles = (): string => `
   }
 
   :root {
-    --kinde-base-font-family: 'Figtree', ${kindeVariables.baseFontFamily};
+    --kinde-base-font-family:  ${kindeVariables.baseFontFamily};
     --kinde-control-select-text-border-color: #d5d7da;
     --kinde-control-select-text-border-color-hover: #d5d7da;
     --kinde-base-font-family: ${kindeVariables.baseFontFamily};
